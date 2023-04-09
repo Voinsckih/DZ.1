c121640c014df362d95f51ad8d79f69308bdd9bc
Voinsckih <129402608+Voinsckih@users.noreply.github.com>
2023-04-09 16:11:35 +0500

    Add files via upload

---
+++ большее из двух/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите первое число (a):");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите второе число (b):");
+        int b = int.Parse(Console.ReadLine());
+
+        
+        if (a>b) {
+            Console.WriteLine("наибольшее число ="+ (a));
+        } else {
+            Console.WriteLine("наибольшее число ="+ (b));
+        }
