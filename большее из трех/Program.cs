23e0295114369ba86920d7aa43d0a3abc59c45e7
Voinsckih <129402608+Voinsckih@users.noreply.github.com>
2023-04-09 16:12:04 +0500

    Add files via upload

---
+++ большее из трех/Program.cs
@@ -0,0 +1,30 @@
+﻿System.Console.WriteLine("Введите первое число "); 
+string s = Console.ReadLine();                     
+int i = int.Parse(s);
+
+System.Console.WriteLine("Введите второе число "); 
+string t = Console.ReadLine();
+int j = int.Parse(t);
+
+System.Console.WriteLine("Введите третье число "); 
+string e = Console.ReadLine();
+int k = int.Parse(e);
+
+int max = 0;
+
+if (max < i)
+{
+    max = i;
+}
+
+if (max < j)
+{
+    max = j;
+}
+
+if (max < k)
+{
+    max = k;
+}
+
+System.Console.WriteLine($"Максимальное число : {max}");
