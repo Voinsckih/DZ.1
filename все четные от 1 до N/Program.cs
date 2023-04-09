8039b58e5d8a68845b9b40bd05a23b5b296d3e50
Voinsckih <129402608+Voinsckih@users.noreply.github.com>
2023-04-09 16:12:53 +0500

    Add files via upload

---
+++ все четные от 1 до N/Program.cs
@@ -0,0 +1,16 @@
+﻿System.Console.WriteLine("Введите число");
+string text = Console.ReadLine();
+int number = int.Parse(text);
+int count = 1;
+
+System.Console.WriteLine($"Четные числа от 1 до {number}: ");
+
+while (count <= number)
+{
+    if (count % 2 == 0)
+    {
+        System.Console.Write($"{count} ");
+        count = count + 1;
+    }
+    count = count +1;
+}
---
+++ четное или нет/Program.cs
@@ -0,0 +1,12 @@
+﻿System.Console.WriteLine("Введите число");
+string text = Console.ReadLine();
+int number = int.Parse(text);
+
+if (number % 2 == 0)
+{
+    System.Console.WriteLine($"Да, {number} - четное число");
+}
+else
+{
+    System.Console.WriteLine($"Нет, {number} - нечетное число");
+}
